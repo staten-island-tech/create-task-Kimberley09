67efcb8735ffa62f4ad687779697fb6137bd1f25
Kimberley09 <kimberleyc8@nycstudents.net>
2025-01-09 13:27:23 -0500

    dc

---
+++ app/src/main.js
@@ -0,0 +1,4 @@
+const bookName = document.getElementById('bookName');
+const authorName = document.getElementById('authorName');
+const publishYear = document.getElementById('publishYear');
+const bookQuantity = document.getElementById('bookQuantity');
